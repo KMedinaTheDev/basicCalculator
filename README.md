278b9b84a28f27cebf1ff862b7a7d7a84a454871
Karen Medina <KMedina.WebDev+github@gmail.com>
2018-03-10 01:37:55 -0500

    added README and css

---
+++ README.md
@@ -0,0 +1,18 @@
+![.:Basic Calculator:.](images/bc.png)
+
+## A Basic Calculator App By Yours Truly
+
+I developed a basic calculator!
+1. Use the input fields to enter numbers
+2. Select the Operator to act between the two inputs
+3. Hit the Clear button to delete both inputs or simply click and type.
+
+## Languages Used
+
+1. JavaScript
+2. HTML
+3. CSS
+
+## Improvements to Be Made:
+
+With more time I would make this app fully responsive to for all devices.
--- main.js
+++ main.js
@@ -1,17 +1,5 @@
 //* build a calculator *\\
 
-//What Can the User Do?
-
-//the User can enter 2 numbers (2 inputs)  and click 1 of 4 buttons(operators)
-
-//What does the User expect?
-  //the User expects a result after entering num1 (operator) num2
-//What does the User see?
-  //the user sees two input fields
-  //the user sees operator buttons (4)-user expects operation
-  //the user sees a result (or answer) after they pick two numbers and either add, subtract, divide, or multiply
-
-alert(">--Don't Sweat Math!---> Simply enter a number in each input field and choose your operator!");
 document.getElementById("divide").onclick=divide;
 document.getElementById("multiply").onclick=multiply;
 document.getElementById("sub").onclick=sub;
